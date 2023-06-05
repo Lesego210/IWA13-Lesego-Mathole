873c65748608153db1b68eccb456fbcdfcb77006
Lesego210 <127973618+Lesego210@users.noreply.github.com>
2023-06-05 14:37:29 +0200

    Add files via upload

---
+++ IWA13/Challenge1.js
@@ -0,0 +1,50 @@
+let state = 'idle'
+let user = null
+let calculated = '1'
+
+// Only allowed to change below
+
+
+const logCalc = () => { 
+    const isString = typeof calculated
+
+   let calculatedAsNumber
+    
+if (isString){
+    calculatedAsNumber = parseInt(calculated)
+
+} else {
+    calculatedAsNumber = calculated 
+}
+    calculated = calculatedAsNumber + 1 
+}
+
+const calcUser = () => {
+  logCalc()
+  if (calculated > 2) user = 'John'
+  if (calculated > 2) state = 'requesting'
+  if (calculated > 3) state = 'idle'
+}
+
+const checkUser = () => {
+	if (user && state === 'requesting') {
+		console.log(`User: ${user} (${calculated})`)
+	}
+}
+
+// Only allowed to change code above
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
